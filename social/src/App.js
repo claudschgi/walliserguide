d7a8abba8b8757a50fd6f714e35e93122f53aefe
Claudia <claudia@cmsbox.ch>
2023-04-15 00:15:53 +0200

    imp new page

--- social/src/App.js
+++ social/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import iconlogo from './images/icon-walliserguide.svg';
+import textlogo from './images/text-walliserguide.svg';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={iconlogo} className="logo logo--icon" alt="logo" />
+        <img src={textlogo} className="logo logo--text" alt="logo" />
+        <p> Dein Guide fürs Wallis. </p>
+        <p> Your Valais Guide. </p>
       </header>
+      <main>
+        <div class="content">
+          <div class="content__wavy"></div>
+          <div class="content__main">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
