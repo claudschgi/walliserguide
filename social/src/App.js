136e9f69f6f72fab675954d2278b16064e8e887c
Claudia <claudia@cmsbox.ch>
2023-04-16 16:55:44 +0200

    add new build 2023-04-16

--- social/src/App.js
+++ social/src/App.js
@@ -1,25 +1,46 @@
 import iconlogo from './images/icon-walliserguide.svg';
 import textlogo from './images/text-walliserguide.svg';
 
-import './App.scss';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={iconlogo} className="logo logo--icon" alt="logo" />
-        <img src={textlogo} className="logo logo--text" alt="logo" />
-        <p> Dein Guide fürs Wallis. </p>
-        <p> Your Valais Guide. </p>
+    <div className="wg-social">
+      <header class="section section--header">
+        <div class="container">
+          <img src={iconlogo} className="logo logo--icon" alt="logo" />
+          <img src={textlogo} className="logo logo--text" alt="logo" />
+          <p> Dein Guide fürs Wallis. </p>
+          <p> Your Valais Guide. </p>
+        </div>
       </header>
-      <main>
+
+    <main>
+      <section class="section section--more">
         <div class="content">
           <div class="content__wavy"></div>
           <div class="content__main">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            <div class="container">
+              <p>Entdecke mehr / Discover more</p>
+              <div className='socialLinks'>
+                <a className="btn btn--primary" href="https://www.walliserguide.ch" target="_blank" rel="noopener noreferrer">
+                  <span className='btn__icon btn__icon--link'></span>Website
+                </a>
+                <a className="btn btn--primary" href="https://www.youtube.com/channel/UC0X0jME-wEQ3DwOZMEAZSrg" target="_blank" rel="noopener noreferrer">
+                  <span className='btn__icon btn__icon--youtube'></span>Youtube
+                </a>
+                <a className="btn btn--primary" href="https://www.tiktok.com/@walliserguide" target="_blank" rel="noopener noreferrer">
+                  <span className='btn__icon btn__icon--tiktok'></span>Tiktok
+                </a>
+                <a className="btn btn--primary" href="https://www.instagram.com/walliserguide/" target="_blank" rel="noopener noreferrer">
+                  <span className='btn__icon btn__icon--instagram'></span>Instagram
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </main>
+      </section>
+    </main>
+
+
     </div>
   );
 }
